c2502de1d0294f283104280db72ad4d51cd88f7f
SammyBloom <nwachiemi@gmail.com>
2021-09-22 00:47:58 +0100

    implemented create react app and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+export const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store ={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 ReactDOM.render(
   <React.StrictMode>
