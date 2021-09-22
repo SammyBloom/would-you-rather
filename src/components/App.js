25aa0a1a45c839211c9791582f97d8946e96f061
SammyBloom <nwachiemi@gmail.com>
2021-09-22 11:37:35 +0100

    implemented 404 page and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import LoadingBar from 'react-redux-loading'
+import Navbar from './Navbar'
+import Login from './Login'
+import Home from './Home'
+import NewQuestion from './NewQuestion'
+import LeaderBoard from './LeaderBoard'
+import Poll from './Poll'
+import ErrorPage from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const { dispatch, loading } = this.props
+    if(loading === true) {
+      dispatch(handleInitialData())
+   }
+  }
+  render() {
+    return (
+    <Router>
+      <Fragment>
+          <Navbar/>
+          <div>
+            <LoadingBar/>
+            { this.props.loading === true
+              ? null
+              : <div>
+              <Switch>
+                <Route path='/login' exact component={Login} />
+                <PrivateRoute path='/' exact component={Home} />
+                <PrivateRoute path='/add' exact component={NewQuestion} />
+                <PrivateRoute path='/questions/:question_id' exact component={Poll} />
+                <PrivateRoute path='/leaderboard' exact component={LeaderBoard} />
+                <Route component={ ErrorPage }/>
+              </Switch>
+            </div>
+              }
+          </div>
+      </Fragment>
+    </Router>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps({users}) {
+  return {
+    loading: isEmpty(users)
+  }
+}
+
+function isEmpty(obj) {
+  for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+          return false
+  }
+  return true
+}
+
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 
-export const store = createStore(reducer, middleware)
+// export const store = createStore(reducer, middleware)
 
-ReactDOM.render(
-  <Provider store ={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store ={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <React.StrictMode>
